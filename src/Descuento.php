bdd4c3efed95d98f5327e2f72d48dc4e4575265e
GID <innovacioncij@gmail.com>
2026-04-19 19:35:29 -0300

    Fix de caso extremo de descuento

--- src/Descuento.php
+++ src/Descuento.php
@@ -4,7 +4,7 @@ namespace App;
 
 class Descuento {
     public function aplicar($precio) {
-        if ($precio > 100) {
+        if ($precio >= 100) {
             return $precio * 0.9;
         }
         return $precio;
